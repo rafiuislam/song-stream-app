c15bfac5f962bb4269e4798c2a7f58f930f0bc4a
Rafiu Islam <md.rafiu.islam@g.bracu.ac.bd>
2023-03-09 12:52:30 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <RouterView />
+</template>
